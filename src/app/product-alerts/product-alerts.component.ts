c3c4ba4b4654649abd02530baa74a924b1e2b366
cleberrodriguesdossantos <cleberrodriguessantos1989@gmail.com>
2021-07-29 11:04:33 -0300

    2 files modified

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -6,9 +6,12 @@ import { Product } from '../products';
   selector: 'app-product-alerts',
   templateUrl: './product-alerts.component.html',
   styleUrls: ['./product-alerts.component.css']
+  
+  
 })
 export class ProductAlertsComponent implements OnInit {
-@Input() product!: Product;
+@Input() product: Product|undefined;
+@Output() notify = new EventEmitter();
   constructor() { }
 
   ngOnInit() {
